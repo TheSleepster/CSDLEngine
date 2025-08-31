8d1112a01c2490d6a518030ebd99a3d9c5663e28
TheSleepster <jal4dsox@live.com>
2025-08-30 20:25:28 -0500

    UNTESTED GCC/Clang x64 intrinsics

--- code/c_intrinsics.h
+++ code/c_intrinsics.h
@@ -1,15 +1,102 @@
-#if !defined(C_INTRINSICS_H)
+#if !defined(C_SRINSICS_H)
 /* ========================================================================
-   $File: c_intrinsics.h $
+   $File: c_srinsics.h $
    $Date: Tue, 29 Jul 25: 05:18PM $
    $Revision: $
    $Creator: Justin Lewis $
    ======================================================================== */
 
-#define C_INTRINSICS_H
+#define C_SRINSICS_H
+
+#include "c_types.h"
+
+/*
+  TODO:
+  - MSVC native srinsics (????)
+  - ARM NEON srinsicS
+ */
 
 #if defined COMPILER_CLANG || defined COMPILER_GCC
-#define popcount32(value) __builtin_popcount(value)
+
+#if ARCH_X64
+    #include <emmintrin.h>
+    #include <xmmintrin.h>
+
+    #define popcount32(value) __builtin_popcount(value)
+
+ /* ===========================================
+    ================== FENCES =================
+    ===========================================*/
+    #define c_atomic_mfence() __atomic_thread_fence(__ATOMIC_SEQ_CST)
+    #define c_atomic_sfence() __atomic_thread_fence(__ATOMIC_RELEASE)
+    #define c_atomic_lfence() __atomic_thread_fence(__ATOMIC_ACQUIRE)
+
+    #define c_mm_mfence() _mm_mfence()
+    #define c_mm_sfence() _mm_sfence()
+    #define c_mm_lfence() _mm_lfence()
+
+ /* =============================================================
+    ====================== ATOMIC INCREMENT =====================
+    ============================================================= */
+    #define c_atomic_increment16(ptr) __atomic_add_fetch((s16*)(ptr), 1, __ATOMIC_SEQ_CST)
+    #define c_atomic_increment32(ptr) __atomic_add_fetch((s32*)(ptr), 1, __ATOMIC_SEQ_CST)
+    #define c_atomic_increment64(ptr) __atomic_add_fetch((s64*)(ptr), 1, __ATOMIC_SEQ_CST)
+
+ /* =============================================================
+    ====================== ATOMIC DECREMENT =====================
+    ============================================================= */
+    #define c_atomic_decrement16(ptr) __atomic_sub_fetch((s16*)(ptr), 1, __ATOMIC_SEQ_CST)
+    #define c_atomic_decrement32(ptr) __atomic_sub_fetch((s32*)(ptr), 1, __ATOMIC_SEQ_CST)
+    #define c_atomic_decrement64(ptr) __atomic_sub_fetch((s64*)(ptr), 1, __ATOMIC_SEQ_CST)
+
+ /* =============================================================
+    ======================== ATOMIC ADD =========================
+    ============================================================= */
+    #define c_atomic_add16(ptr, value) __atomic_fetch_add((s16*)(ptr), (s16)value, __ATOMIC_SEQ_CST)
+    #define c_atomic_add32(ptr, value) __atomic_fetch_add((s32*)(ptr), (s32)value, __ATOMIC_SEQ_CST)
+    #define c_atomic_add64(ptr, value) __atomic_fetch_add((s64*)(ptr), (s64)value, __ATOMIC_SEQ_CST)
+
+ /* =============================================================
+    ====================== ATOMIC SUBTRACT ======================
+    ============================================================= */
+    #define c_atomic_sub16(ptr, value) __atomic_fetch_sub((s16*)(ptr), (s16)value, __ATOMIC_SEQ_CST)
+    #define c_atomic_sub32(ptr, value) __atomic_fetch_sub((s32*)(ptr), (s32)value, __ATOMIC_SEQ_CST)
+    #define c_atomic_sub64(ptr, value) __atomic_fetch_sub((s64*)(ptr), (s64)value, __ATOMIC_SEQ_CST)
+
+ /* =============================================================
+    ====================== ATOMIC EXCHANGE ======================
+    ============================================================= */
+    #define c_atomic_exchange16(ptr, val) __atomic_exchange_n((s16*)(ptr), (s16)(val), __ATOMIC_SEQ_CST)
+    #define c_atomic_exchange32(ptr, val) __atomic_exchange_n((s32*)(ptr), (s32)(val), __ATOMIC_SEQ_CST)
+    #define c_atomic_exchange64(ptr, val) __atomic_exchange_n((s64*)(ptr), (s64)(val), __ATOMIC_SEQ_CST)
+
+ /* =============================================================
+    =================== ATOMIC COMPARE EXCHANGE =================
+    ============================================================= */
+    #define c_atomic_compare_exchange16(ptr, exchange, comparand) ({    \
+        s16 _expected = (int16_t)(comparand    );                       \
+        __atomic_compare_exchange_n((s16*)(ptr), &_expected,            \
+                                    (s16)(exchange), 0,                 \
+                                    __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);\
+        _expected;                                                      \
+    })
+
+    #define c_atomic_compare_exchange32(ptr, exchange, comparand) ({    \
+        s32 _expected = (int32_t)(comparand);                           \
+        __atomic_compare_exchange_n((s32*)(ptr), &_expected,            \
+                                    (s32)(exchange), 0,                 \
+                                    __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);\
+        _expected;                                                      \
+    })
+
+    #define c_atomic_compare_exchange64(ptr, exchange, comparand) ({    \
+        int64_t _expected = (s64)(comparand);                           \
+        __atomic_compare_exchange_n((s64*)(ptr), &_expected,            \
+                                    (s64)(exchange), 0,                 \
+                                    __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);\
+        _expected;                                                      \
+    })
+#endif
 
 #elif defined COMPILER_CL
 #endif
--- code/main.cpp
+++ code/main.cpp
@@ -60,6 +60,47 @@
 
 global bool8 running;
 
+typedef struct multithreading_work_queue multithreading_work_queue_t;
+#define WORK_QUEUE_ENTRY_CALLBACK(name) void name(void *user_data);
+typedef WORK_QUEUE_ENTRY_CALLBACK(work_queue_callback_t);
+
+typedef struct multithreading_work_queue_entry
+{
+    bool8                  is_valid;
+
+    void                  *user_data;
+    work_queue_callback_t *callback;
+}multithreading_work_queue_entry_t;
+
+typedef struct multithreading_work_queue
+{
+    u32 volatile                      completion_goal;
+    u32 volatile                      next_entry_to_read;
+    u32 volatile                      next_entry_to_write;
+    u32 volatile                      total_jobs_completed;
+
+    multithreading_work_queue_entry_t entries[512];
+    os_semaphore_t                    semaphore;
+}multithreading_work_queue_t;
+
+typedef struct multithreading_work_queue_manager
+{
+    multithreading_work_queue_t high_priority_queue;
+    multithreading_work_queue_t low_priority_queue;
+}multithreading_work_queue_manager_t;
+
+internal bool8
+c_add_entry_to_work_queue(multithreading_work_queue_t *queue, work_queue_callback_t *callback, void *user_data)
+{
+    bool8 result = true;
+
+    // new_entry.callback  = callback;
+    // new_entry.user_data = user_data;
+    // new_entry.is_valid  = true;
+
+    return(result);
+}
+
 #ifdef INTERNAL_DEBUG
 FILE_WATCHER_CALLBACK(test_callback)
 {
