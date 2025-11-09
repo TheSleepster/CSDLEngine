75fa236785a21a90a868658e1e0e7351caeea14b
Lewis <ibjal20@outlook.com>
2025-11-09 16:37:30 -0600

    dashing

--- code/g_main.cpp
+++ code/g_main.cpp
@@ -19,6 +19,40 @@ constexpr u32     MAX_ENTITIES   = 10000;
 
 global float32 dt_accumulator = 0.0f;
 
+struct tick_clock_t
+{
+    bool32 begun;
+
+    u32    start_tick;
+    u32    end_tick;
+
+    u32    elapsed;
+    u32    duration;
+};
+
+struct sprite_t
+{
+    asset_handle_t texture;
+    vec2_t         size;
+    u32            frame_count;
+
+    string_t       name;
+};
+
+enum e_collision_masking
+{
+    ECM_Player      = 1 << 0,
+    ECM_Map         = 1 << 1,
+    ECM_AllEntities = 1 << 2,
+};
+
+struct collision_box
+{
+    bool32       is_active;
+    u32          collision_mask;
+    rectangle2_t rect;
+};
+
 enum entity_type
 {
     ET_Player,
@@ -34,33 +68,18 @@ enum entity_flags
     EF_Alive    = 1 << 4,
     EF_Gravitic = 1 << 5,
     EF_Actor    = 1 << 6,
+    EF_Static   = 1 << 7,
     EF_Count
-};
-
-struct sprite_t
-{
-    asset_handle_t texture;
-    vec2_t         size;
-    u32            frame_count;
-
-    string_t       name;
-};
-
-struct event_clock_t
-{
-    float32 start_time;
-    float32 end_time;
-
-    float32 elapsed;
-    float32 duration;
 };
 
 struct entity_t
 {
     u64           e_flags;
+    u32           e_ID;
     u32           e_type;
     u32           render_options;
 
+    collision_box hit_box;
     sprite_t      sprite;
     s32           facing_dir;
 
@@ -72,8 +91,13 @@ struct entity_t
     float32       gravity_intensity;
     float32       friction_scale;
     vec2_t        velocity;
+ 
+    tick_clock_t  dash_clock;
+    u32           dash_counter;
+    u32           max_dashes;
 
-    event_clock_t dash_timer;
+    u32           jump_counter;
+    u32           max_jumps;
 
     vec2_t        position;
     vec2_t        prev_position;
@@ -93,9 +117,7 @@ struct entity_manager_t
 
 struct game_state_t 
 {
-    bool8  is_initialized;
-    u32    play_state;
-
+    bool8               is_initialized;
     input_controller_t *controller;
     render_group_t     *entity_render_group;
     entity_manager_t    entity_manager;
@@ -104,6 +126,7 @@ struct game_state_t
     vec2_t              input_axis;
     bool8               should_jump;
     bool8               should_dash;
+    u64                 simulation_tick;
 
     file_t              input_data_file;
     bool8               recording_input;
@@ -126,6 +149,7 @@ entity_create(entity_manager_t *entity_manager)
         if(!(entity->e_flags & EF_Valid))
         {
             new_entity = entity;
+            new_entity->e_ID = entity_manager->active_entity_count;
             break;
         }
     }
@@ -164,23 +188,30 @@ entity_player_create(entity_manager_t *entity_manager, asset_manager_t *asset_ma
     entity_t *player = entity_create(entity_manager);
     ZeroStruct(*player);
 
-    player->e_type              = ET_Player;
-    player->e_flags            |= (EF_Alive|EF_Gravitic|EF_Actor);
-    player->sprite              = entity_init_sprite_data(asset_manager, STR("player"));
-    player->render_size         = player->sprite.size;
-    player->render_size         = vec2(10, 10);
-    player->render_color        = COLOR_WHITE;
-    player->position            = position;
+    player->e_type                = ET_Player;
+    player->e_flags              |= (EF_Alive|EF_Gravitic|EF_Actor);
+    player->sprite                = entity_init_sprite_data(asset_manager, STR("player"));
+    player->render_size           = vec2(10, 10);
+    player->render_color          = COLOR_WHITE;
+    player->position              = position;
 
-    player->x_accel             =  20.0f;
-    player->movement_speed      =  2.0f;
-    player->terminal_velocity   =  5.0f; 
-    player->jump_speed          =  300.0f;
-    player->dash_speed          =  300.0f;
-    player->friction_scale      =  0.50f;
-    player->gravity_intensity   = -10.0f;
+    player->hit_box               = rect2_create(player->position, player->render_size);
+    player->hitbox.collision_mask = ECM_AllEntities;
 
-    player->dash_timer.duration = 0.5f;
+    player->x_accel               =  20.0f;
+    player->movement_speed        =  2.0f;
+    player->terminal_velocity     =  5.0f; 
+    player->jump_speed            =  300.0f;
+    player->dash_speed            =  300.0f;
+    player->friction_scale        =  0.50f;
+    player->gravity_intensity     = -10.0f;
+
+    player->dash_clock.duration   = 15;
+    player->dash_counter          = 1;
+    player->max_dashes            = 1;
+
+    player->jump_counter          = 1;
+    player->max_jumps             = 1;
 
     return(player);
 }
@@ -205,6 +236,8 @@ entity_render(render_state_t *render_state, asset_manager_t *asset_manager, enti
 internal void
 game_simulate(vec2_t input_axis, float32 delta_time)
 {
+    ++global_game_state.simulation_tick;
+
     // NOTE(Sleepster): entity simulate loop
     for(u32 entity_index = 0;
         entity_index <= global_game_state.entity_manager.active_entity_count;
@@ -223,6 +256,8 @@ game_simulate(vec2_t input_axis, float32 delta_time)
 
                 if(entity->is_on_ground)
                 {
+                    global_game_state.should_dash = false;
+                    entity->dash_clock.begun      = false;
                     if(input_axis.x != 0.0f)
                     {
                         entity->velocity.x += (input_axis.x * (entity->x_accel * delta_time));
@@ -231,24 +266,49 @@ game_simulate(vec2_t input_axis, float32 delta_time)
                         if(entity->velocity.x < -entity->movement_speed) entity->velocity.x = -entity->movement_speed;
                     }
 
-                    if(global_game_state.should_jump)
+                    if(global_game_state.should_jump && entity->jump_counter > 0)
                     {
                         entity->is_on_ground = false;
                         entity->velocity.y   = entity->jump_speed * delta_time; 
 
+                        --entity->jump_counter;
                         global_game_state.should_jump = false;
                     }
                 }
                 else
                 {
-                    if(global_game_state.should_dash)
+                    if(global_game_state.should_dash && entity->dash_counter > 0)
                     {
-                        entity->velocity.x = entity->facing_dir * (entity->dash_speed * delta_time);
-                        global_game_state.should_dash = false;
+                        if(!entity->dash_clock.begun)
+                        {
+                            entity->dash_clock.start_tick = global_game_state.simulation_tick;
+                            entity->dash_clock.elapsed    = 0;
+                            entity->dash_clock.begun      = true;
+                        }
+
+                        if(entity->dash_clock.elapsed < entity->dash_clock.duration)
+                        {
+                            entity->velocity.x = entity->facing_dir * (entity->dash_speed * delta_time);
+                            entity->velocity.y = input_axis.y * (entity->dash_speed * delta_time);
+
+                            entity->dash_clock.elapsed += 1;
+                        }
+                        else
+                        {
+                            global_game_state.should_dash = false;
+                            entity->dash_clock.begun      = false;
+
+                            --entity->dash_counter;
+                        }
+                    }
+
+                    if(input_axis.x != 0.0f)
+                    {
+                        entity->velocity.x += (input_axis.x * ((entity->x_accel * 0.25) * delta_time));
                     }
                 }
 
-                if((entity->e_flags & EF_Gravitic) != 0)
+                if(((entity->e_flags & EF_Gravitic) != 0) && (!entity->dash_clock.begun))
                 {
                     entity->velocity.y += (entity->gravity_intensity * delta_time);
                 }
@@ -264,6 +324,9 @@ game_simulate(vec2_t input_axis, float32 delta_time)
                 {
                     entity->is_on_ground = true;
                     entity->position.y   = 0.0f;
+
+                    entity->jump_counter = entity->max_jumps;
+                    entity->dash_counter = entity->max_dashes;
                 }
 
             }
